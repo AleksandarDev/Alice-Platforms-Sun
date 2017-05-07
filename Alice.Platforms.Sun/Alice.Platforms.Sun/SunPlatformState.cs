c673145e416291c4aa4d36a68c1fd5155d956956
Aleksandar Toplek <aleksandar.toplek@gmail.com>
2017-05-07 18:33:25 +0200

    Added state

---
+++ Alice.Platforms.Sun/Alice.Platforms.Sun/SunPlatformState.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Alice.Platforms.Base;
+
+namespace Alice.Platforms.Sun
+{
+    public class SunPlatformState : PlatformState
+    {
+        public TimeSpan SunsetTime
+        {
+            get => this.GetProperty<TimeSpan>();
+            set => this.SetProperty(value);
+        }
+
+        public TimeSpan SunriseTime
+        {
+            get => this.GetProperty<TimeSpan>();
+            set => this.SetProperty(value);
+        }
+
+        public SunStates Current
+        {
+            get => this.GetProperty<SunStates>();
+            set => this.SetProperty(value);
+        }
+
+
+        public SunPlatformState(IState state) : base(state)
+        {
+        }
+    }
+}
---
+++ Alice.Platforms.Sun/Alice.Platforms.Sun/SunStates.cs
@@ -0,0 +1,10 @@
+﻿namespace Alice.Platforms.Sun
+{
+    public enum SunStates
+    {
+        Day,
+        Night,
+        Sunset,
+        Sunrise
+    }
+}
